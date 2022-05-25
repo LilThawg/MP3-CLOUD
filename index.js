174f2a9994fc2ff3b4dbbafafea15d7688268ac6
LilThawg <wronglove0301@gmail.com>
2022-05-26 03:24:46 +0700

    my changes

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ app.post("/UPLOAD", async (req, res) => {
   }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
